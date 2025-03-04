a346ffe7a7592e7083e2c54103bf0483061fa815
spohlman <spohlman@diocesan.com>
2025-03-04 14:36:57 -0500

    calendar homepage

--- acf/tabs/calendar.php
+++ acf/tabs/calendar.php
@@ -20,6 +20,40 @@ function acf_calendar()
             'endpoint' => 0,
         ),
         array(
+            'key' => 'field_623cb1cbaac8z',
+            'label' => 'Heading',
+            'name' => 'calendar_heading',
+            'type' => 'text',
+            'instructions' => '*Max 20 Characters',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '33',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => 20,
+        ),
+        array(
+            'key' => 'field_62d586628366z',
+            'label' => 'Link',
+            'name' => 'calendar_link',
+            'type' => 'link',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '33',
+                'class' => '',
+                'id' => '',
+            ),
+            'return_format' => 'array',
+        ),
+        array(
             'key' => 'field_626aca56f562c',
             'label' => 'Calendar',
             'name' => 'calendar',
@@ -28,11 +62,11 @@ function acf_calendar()
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '20',
+                'width' => '33',
                 'class' => '',
                 'id' => '',
             ),
             'allow_null' => 1,
-        ),
+        )
     );
 }
