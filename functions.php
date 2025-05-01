f3d6e3314d515151c957fd3940e7335bcb1c0911
spohlman <spohlman@diocesan.com>
2025-05-01 15:37:00 -0400

    Match homepage and inner page buttons

--- functions.php
+++ functions.php
@@ -53,6 +53,18 @@ function getDefaultPlaceholderImage()
     return get_template_directory_uri() . "/assets/img/patrick_placeholder.png";
 }
 
+function add_custom_class_to_buttons($block_content, $block)
+{
+    // Check if the block is a button block
+    if ('core/button' === $block['blockName']) {
+        // Add your custom class to the block
+        $block_content = str_replace('wp-block-button__link', 'wp-block-button__link the-button', $block_content);
+    }
+
+    return $block_content;
+}
+add_filter('render_block', 'add_custom_class_to_buttons', 10, 2);
+
 require get_template_directory() . "/update-checker/plugin-update-checker.php";
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
